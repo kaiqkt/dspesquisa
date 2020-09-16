1a0ad4db6114b6f0d800fcd6f50251e4f3570614
kaiquepill <kaique.gomes@bandtec.com.br>
2020-09-16 19:37:50 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/DssuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dspesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DssuperiorApplication.class, args);
+	}
+
+}
