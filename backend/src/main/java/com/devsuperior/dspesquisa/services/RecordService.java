78fabeec132daba785065cc58b6caa0cfd0420b1
kaiquepill <kaique.gomes@bandtec.com.br>
2020-09-17 17:47:03 -0300

    GET Records

--- backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
@@ -7,10 +7,13 @@ import com.devsuperior.dspesquisa.entities.Record;
 import com.devsuperior.dspesquisa.repositories.GameRepository;
 import com.devsuperior.dspesquisa.repositories.RecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.List;
 
 @Service
 public class RecordService {
@@ -33,5 +36,10 @@ public class RecordService {
 
         entity = recordRepository.save(entity);
         return new RecordDto(entity);
+    }
+
+    @Transactional
+    public Page<RecordDto> findByMoments(Instant minDate, Instant maxDate, PageRequest pageRequest) {
+        return recordRepository.findByMoments(minDate, maxDate, pageRequest).map(x -> new RecordDto(x));
     }
 }
