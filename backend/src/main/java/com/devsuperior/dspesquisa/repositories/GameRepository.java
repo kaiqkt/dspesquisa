1f7b64ece6f1188135a74de598bf964174667457
kaiquepill <kaique.gomes@bandtec.com.br>
2020-09-17 15:57:04 -0300

    DTO, Service

--- backend/src/main/java/com/devsuperior/dspesquisa/repositories/GameRepository.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/GameRepository.java
@@ -1,8 +1,10 @@
-package com.devsuperior.dspesquisa.entities.repositories;
+package com.devsuperior.dspesquisa.repositories;
 
 import com.devsuperior.dspesquisa.entities.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
 
 }
---
+++ backend/src/main/java/com/devsuperior/dspesquisa/resources/GameResource.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dspesquisa.resources;
+
+import com.devsuperior.dspesquisa.dto.GameDto;
+import com.devsuperior.dspesquisa.entities.Game;
+import com.devsuperior.dspesquisa.repositories.GameRepository;
+import com.devsuperior.dspesquisa.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameResource {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public ResponseEntity<List<GameDto>> findAll() {
+        List<GameDto> list = gameService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
